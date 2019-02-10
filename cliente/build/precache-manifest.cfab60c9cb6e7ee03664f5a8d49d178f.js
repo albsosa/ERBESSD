c6d156c020125a84437b39c0de7e17649fe2e9f1
albsosa <inf.albsosa@gmail.com>
2019-02-09 22:21:27 -0600

    version para probar en un servidor

---
+++ cliente/build/precache-manifest.cfab60c9cb6e7ee03664f5a8d49d178f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e29420f385322d7e6934",
+    "url": "/static/js/main.e29420f3.chunk.js"
+  },
+  {
+    "revision": "43da78c5a905cb021393",
+    "url": "/static/js/1.43da78c5.chunk.js"
+  },
+  {
+    "revision": "e29420f385322d7e6934",
+    "url": "/static/css/main.1cebb802.chunk.css"
+  },
+  {
+    "revision": "8f743259d27c45564c766ad546a87815",
+    "url": "/index.html"
+  }
+];
